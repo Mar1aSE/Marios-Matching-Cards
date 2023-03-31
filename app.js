a8645fe684c7093dfcbc2871dfbfd86531291496
mar1ase <lupita31311@gmail.com>
2023-03-31 10:14:56 -0500

    almost done

--- app.js
+++ app.js
@@ -1,48 +1,82 @@
-let board, flipped, matches;
+let flipped, matches, firstClick, secondClick;
 
 const cardsEls = document.querySelectorAll('.back') 
-// console.log(cardsEls)
+
 const scoreEl = document.querySelector('.scoreBoard')
 
+const cardEl = document.querySelectorAll('.card')
+
 const resetBtnEl = document.querySelector('button').addEventListener("click", init)
+
 function init() {
-    board = []
     flipped = false
     matches = 0 
-cardsEls.forEach((c => c.addEventListener('click', firstCard)))
+    scoreEl.innerHTML = `Matches: ${matches}`
+
+    cardsEls.forEach((c) => {
+        c.classList.add('back')
+        c.setAttribute('src', './imgs/back-card.png')
+
+    }) 
+
+   cardsEls.forEach((c => c.addEventListener('click', clicked)))
 }
 init()
 
 
-//clickedCArd.parentElement.firstElementChild.id 
-
-function firstCard(event) {
-let clickedCArd = event.target
-
-console.dir(clickedCArd)
-clickedCArd.classList.remove('back')
-clickedCArd.setAttribute('src', '')
-
+function clicked(event) {
+// let clickedCArd = event.target
+if (flipped === false){
+    firstClick = event.target
+    firstClick.classList.remove('back')
+    firstClick.setAttribute('src', '')
+    flipped = true 
+} else if (flipped === true) {
+    secondClick = event.target
+    secondClick.classList.remove('back')
+    secondClick.setAttribute('src', '')
+    flipped = false
 check()
-function check() {
-   if (!board.includes(clickedCArd.parentElement.firstElementChild.id)){
-        board.push(clickedCArd.parentElement.firstElementChild.id )
-        if (board.length !== 1 ) {
-            // init()
-         setTimeout(() => {
-            showBack()
-         }, 1000)
-         function showBack(click){  
-            clickedCArd.setAttribute('src', './imgs/back-card.png')
-         clickedCArd.classList.add('back')
-          }
-         board= []
-        } 
-    } 
-    else if (board.includes(clickedCArd.parentElement.firstElementChild.id)) {
-        console.log('it is a match')
-        board =[]
-    } 
-
 }
-} 
+}
+
+
+function check() {
+if (firstClick.id === secondClick.id) {
+    firstClick.removeEventListener('clicked', clicked)
+    secondClick.removeEventListener('clicked', clicked)
+    matches++ 
+    scoreEl.innerHTML = `Matches: ${matches}`
+}else if (firstClick.id !== secondClick.id) {
+    setTimeout (() => {
+        firstClick.classList.add('back')
+        secondClick.classList.add('back')
+        firstClick.setAttribute('src' , './imgs/back-card.png')
+        secondClick.setAttribute('src', './imgs/back-card.png')
+
+    }, 1500)
+}
+
+
+
+
+    // if (!board.includes(clickedCArd.parentElement.firstElementChild.id)){
+    //     board.push(clickedCArd.parentElement.firstElementChild.id )
+    //     if (board.length !== 1 ) {
+    //         // init()
+    //         setTimeout(() => {
+    //             showBack()
+    //         }, 1000)
+    //         function showBack(click){  
+    //             clickedCArd.setAttribute('src', './imgs/back-card.png')
+    //             clickedCArd.classList.add('back')
+    //         }
+    //         board= []
+    //     } 
+    // } 
+    // else if (board.includes(clickedCArd.parentElement.firstElementChild.id)) {
+    //     console.log('it is a match')
+    //     board =[]
+    // } 
+    
+}
